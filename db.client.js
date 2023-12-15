6c949346c2f2eea4c2adaaef38674b66f27069c5
LEONETTI Cédric <146001146+Cedric666666@users.noreply.github.com>
2023-12-15 11:24:37 +0100

    Update db.client.js

--- db.client.js
+++ db.client.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  'postgres://data_base_08532_user:bZUmNb0kfSmNvXMnM3HmVgG7TJDYeN9f@dpg-clu2f3i1hbls73e8mskg-a.frankfurt-postgres.render.com/data_base_08532', // TODO
   {
     dialectOptions: {
       ssl: {
